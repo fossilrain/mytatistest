875477a482817fb96b229d11d81f874ffd4f66e7
RainFossil <RainFossil@RainFossil-PC>
2016-12-27 20:30:22 +0800

    add dbscript

---
+++ cfg/mybatistest.sql
@@ -0,0 +1,86 @@
+/*
+SQLyog Ultimate v11.24 (32 bit)
+MySQL - 5.5.48 : Database - mybatistest
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`mybatistest` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `mybatistest`;
+
+/*Table structure for table `item` */
+
+DROP TABLE IF EXISTS `item`;
+
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `detail` varchar(300) DEFAULT NULL,
+  `createtime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `item` */
+
+insert  into `item`(`id`,`name`,`price`,`detail`,`createtime`) values (1,'item23',23,'商品描述23','2016-09-12 21:11:05'),(2,'name2',2.2,'商品描述2','2016-09-11 17:14:48'),(3,'item3',3,'商品描述3','2016-09-12 20:35:11');
+
+/*Table structure for table `orderdetail` */
+
+DROP TABLE IF EXISTS `orderdetail`;
+
+CREATE TABLE `orderdetail` (
+  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `order_id` int(9) DEFAULT NULL,
+  `item_id` int(11) DEFAULT NULL,
+  `count_buy` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `orderdetail` */
+
+insert  into `orderdetail`(`id`,`order_id`,`item_id`,`count_buy`) values (1,1,2,5),(2,2,1,6),(3,2,3,7),(4,3,2,3);
+
+/*Table structure for table `orders` */
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE `orders` (
+  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `user_id` int(9) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Data for the table `orders` */
+
+insert  into `orders`(`id`,`user_id`,`create_date`) values (1,2,'2016-09-18 20:46:22'),(2,2,'2016-09-18 20:46:32'),(3,1,'2016-09-18 20:46:36');
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(9) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `sex` varchar(1) DEFAULT NULL,
+  `address` varchar(300) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`name`,`sex`,`address`) values (1,'user1','男','地址1'),(2,'user2','女','地址2');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
